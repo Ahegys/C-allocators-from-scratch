9287b2f39fe715347c9597400a6614b7eca4673d
Ahegys <andersonfelpe123@gmail.com>
2025-02-24 01:27:20 -0300

    create tuple from scratch

---
+++ src/util.h
@@ -0,0 +1,12 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#define DEFINE_TUPLE(name, error_type, error_name, data_type, data_name) \
+typedef struct { \
+    error_type error_name; \
+    data_type data_name; \
+} name;
+
+DEFINE_TUPLE(ArenaResult, int*, error, void*, data)
+
+#endif // __UTIL_H__
